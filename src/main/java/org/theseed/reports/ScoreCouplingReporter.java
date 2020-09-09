e4f77a5f088475fe44ce4b7de1086ffa86f6a185
Bruce Parrello <bparrello@figresearch.com>
2020-09-08 23:11:40 -0500

    Refactoring.

--- src/main/java/org/theseed/reports/ScoreCouplingReporter.java
+++ src/main/java/org/theseed/reports/ScoreCouplingReporter.java
@@ -15,6 +15,7 @@ import org.theseed.genome.Feature;
 import org.theseed.genome.Genome;
 import org.theseed.genome.coupling.BaseCouplingProcessor;
 import org.theseed.genome.coupling.FeatureClass;
+import org.theseed.proteins.Function;
 import org.theseed.sequence.ProteinKmers;
 import org.theseed.sequence.SequenceKmers;
 
@@ -147,7 +148,7 @@ public class ScoreCouplingReporter extends CouplingReporter {
         for (Feature feat : feats) {
             // Check for the seed protein.  We keep the longest.
             String function = feat.getFunction();
-            if (function != null && function.contentEquals(SEED_PROTEIN_FUNCTION)) {
+            if (function != null && Function.commentFree(function).contentEquals(SEED_PROTEIN_FUNCTION)) {
                 String newProt = feat.getProteinTranslation();
                 if (newProt != null && newProt.length() > prot.length()) prot = newProt;
             }
