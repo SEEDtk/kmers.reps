ad95b6f002a03173ec2707dcbc08e59b3538629c
Bruce Parrello <bparrello@figresearch.com>
2021-03-17 23:53:11 -0500

    Genome source support.

--- src/main/java/org/theseed/proteins/kmers/reps/SeqTableProcessor.java
+++ src/main/java/org/theseed/proteins/kmers/reps/SeqTableProcessor.java
@@ -4,20 +4,18 @@
 package org.theseed.proteins.kmers.reps;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
 import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.Option;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.theseed.genome.Feature;
 import org.theseed.genome.Genome;
-import org.theseed.genome.GenomeDirectory;
-import org.theseed.locations.Location;
+import org.theseed.genome.iterator.GenomeSource;
 import org.theseed.proteins.Function;
 import org.theseed.utils.ParseFailureException;
 import org.theseed.utils.RestartableBaseProcessor;
@@ -46,6 +44,8 @@ public class SeqTableProcessor extends RestartableBaseProcessor {
     // FIELDS
     /** logging facility */
     protected static Logger log = LoggerFactory.getLogger(SeqTableProcessor.class);
+    /** input genome source */
+    private GenomeSource genomes;
     /** match pattern for seed protein */
     public static final Pattern SEED_PROTEIN = Pattern.compile("Phenylalanyl-tRNA\\s+synthetase\\s+alpha\\s+chain(?:\\s+\\(E[^)]+\\))?", Pattern.CASE_INSENSITIVE);
     /** list of sequence names */
@@ -53,21 +53,23 @@ public class SeqTableProcessor extends RestartableBaseProcessor {
 
     // COMMAND-LINE OPTIONS
 
-    /** list of genome directories to process */
-    @Argument(index = 0, metaVar = "gtoDir1 gtoDir2 ...", usage = "GTO directories to scan")
-    private List<File> gtoDirs;
+    /** type of genome source (master directory, normal, PATRIC input) */
+    @Option(name = "--type", usage = "type of input (master directory, GTO directory, PATRIC ID file)")
+    private GenomeSource.Type inType;
+
+    /** genome source to process */
+    @Argument(index = 0, metaVar = "gtoDir", usage = "genome directory to scan")
+    private File gtoDir;
 
     @Override
     protected void setDefaults() {
+        this.inType = GenomeSource.Type.DIR;
     }
 
     @Override
     protected boolean validateParms() throws IOException, ParseFailureException {
-        // Verify each of the genome directories.
-        for (File gtoDir : gtoDirs) {
-            if (! gtoDir.isDirectory())
-                throw new FileNotFoundException("Input directory " + gtoDir + " is not found or invalid.");
-        }
+        // Verify the input genome source.
+        this.genomes = this.inType.create(this.gtoDir);
         // Setup the output/resume file.
         String header = "genome_id\t" + StringUtils.join(funSeqNames, "\t");
         this.setup(header, "genome_id");
@@ -78,30 +80,26 @@ public class SeqTableProcessor extends RestartableBaseProcessor {
     protected void runCommand() throws Exception {
         int badCount = 0;
         int totCount = 0;
-        for (File gtoDir : this.gtoDirs) {
-            log.info("Processing directory {}.", gtoDir);
-            GenomeDirectory genomes = new GenomeDirectory(gtoDir);
-            for (Genome genome : genomes) {
-                String genomeId = genome.getId();
-                if (! this.isProcessed(genomeId)) {
-                    String[] seqsFound = this.processGenome(genome);
-                    // Verify that we found everything.  We do this the slow way to provide more useful
-                    // error messages.
-                    boolean ok = true;
-                    for (int i = 0; i < funSeqNames.length; i++) {
-                        if (seqsFound[i].isEmpty()) {
-                            ok = false;
-                            log.warn("WARNING: {} is missing a {} sequence.", genome, funSeqNames[i]);
-                        }
+        for (Genome genome : genomes) {
+            String genomeId = genome.getId();
+            if (! this.isProcessed(genomeId)) {
+                String[] seqsFound = this.processGenome(genome);
+                // Verify that we found everything.  We do this the slow way to provide more useful
+                // error messages.
+                boolean ok = true;
+                for (int i = 0; i < funSeqNames.length; i++) {
+                    if (seqsFound[i].isEmpty()) {
+                        ok = false;
+                        log.warn("WARNING: {} is missing a {} sequence.", genome, funSeqNames[i]);
                     }
-                    if (ok)
-                        this.println(genomeId + "\t" + StringUtils.join(seqsFound, "\t"));
-                    else
-                        badCount++;
-                    this.markProcessed(genomeId);
                 }
-                totCount++;
+                if (ok)
+                    this.println(genomeId + "\t" + StringUtils.join(seqsFound, "\t"));
+                else
+                    badCount++;
+                this.markProcessed(genomeId);
             }
+            totCount++;
         }
         log.info("{} of {} genomes were missing sequences.", badCount, totCount);
     }
@@ -118,29 +116,20 @@ public class SeqTableProcessor extends RestartableBaseProcessor {
         // Initalize the output.
         String[] retVal = new String[funSeqNames.length];
         Arrays.fill(retVal, "");
-        // Loop through the features.
+        // Loop through the pegs.
         log.info("Searching {}.", genome);
-        for (Feature feat : genome.getFeatures()) {
+        for (Feature feat : genome.getPegs()) {
             String function = Function.commentFree(feat.getPegFunction());
-            switch (feat.getType()) {
-            case "CDS" :
-                // For a peg, we check for the seed protein.
-                if (SEED_PROTEIN.matcher(function).matches()) {
-                    String proposed = feat.getProteinTranslation();
-                    if (proposed.length() > retVal[0].length())
-                        retVal[0] = proposed;
-                }
-                break;
-            case "rna" :
-                // For an RNA, we check for an SSU.
-                if (Genome.SSU_R_RNA.matcher(function).find()) {
-                    Location loc = feat.getLocation();
-                    if (loc.getLength() > retVal[1].length())
-                        retVal[1] = genome.getDna(feat.getLocation());
-                }
-                break;
+            // For a peg, we check for the seed protein.
+            if (SEED_PROTEIN.matcher(function).matches()) {
+                String proposed = feat.getProteinTranslation();
+                if (proposed.length() > retVal[0].length())
+                    retVal[0] = proposed;
             }
         }
+        // Get the SSU rRNA.
+        retVal[1] = genome.getSsuRRna();
+        // Return both sequences.
         return retVal;
     }
 
