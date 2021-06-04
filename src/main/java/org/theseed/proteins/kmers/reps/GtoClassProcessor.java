be03e58efe23cc151c2591bdc0450f9a0b82909b
Bruce Parrello <bparrello@figresearch.com>
2021-06-04 10:37:40 -0500

    Removed unused import.

--- src/main/java/org/theseed/proteins/kmers/reps/GtoClassProcessor.java
+++ src/main/java/org/theseed/proteins/kmers/reps/GtoClassProcessor.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
